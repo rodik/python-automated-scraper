7d5451fca96ad3a0a2146595d4a3ab2053c2205a
rodik <filip.rodik@gmail.com>
2022-02-27 23:27:58 +0000

    upgraded scraper, add workflow

--- README.md
+++ README.md
@@ -1,4 +1,54 @@
 # python-automated-scraper
-example scraper automated with Workflows
+Dev Container for Python scraper automated with [Github Actions](https://docs.github.com/en/actions)
 
-# add something
+# About
+
+The goal of this project is to demonstrate a method for automating Python scrapers using Github Actions. It is part of [OpenDataDay-2022](http://odd.codeforcroatia.org/) program by [CodeForCroatia](https://codeforcroatia.org/). Target audience is a mix of technical and non-technical users. The instructions are designed to help new developers get started.
+
+To simplify setup, a VS Code [development container](https://code.visualstudio.com/docs/remote/containers) is created.
+## Components
+
+### Scraper
+
+A simple Python scraper to get announcements from official Government of the Republic of Croatia [pages](https://vlada.gov.hr/) is provided. Python code is basic as the scraper is not the main focus of this demo. </br>
+For more information on what scraping is start [here](https://en.wikipedia.org/wiki/Web_scraping).
+
+### Data destination
+
+Collected data needs to be stored in a database. Various types of databases can be used for this purpose. Some simple solutions include:
+- Structured text files (CSV, JSON)
+- Spreadsheets (Excel, Google spreadsheet)
+- Your email inbox :)
+
+This demo will show how to use a Google spreadsheet to write rows of data each time new data is available. This solution includes additional setup as the scraper must have access to this spreadsheet in your Google Drive.
+
+### Automation
+Github Workflow [file](/.github/workflows/run_scraper.yml) contains logic to execute scraper code on a schedule. It also allows manual runs [here](https://github.com/rodik/python-automated-scraper/actions/workflows/run_scraper.yml).
+
+
+
+# Setup instructions
+
+Start by creating a GitHub account if you don't have one.
+
+1. [Fork](https://docs.github.com/en/get-started/quickstart/fork-a-repo) this repository (_rodik/python-automated-scraper_).
+1. Installation [instructions](https://code.visualstudio.com/docs/remote/containers#_installation) for local machine requirements:
+    - Docker Desktop
+    - Visual Studio Code (VS Code)
+    - Remote Development [Extension](https://code.visualstudio.com/docs/editor/extension-marketplace#_browse-for-extensions) pack
+1. Open VS Code
+    - Open the Command Palette (F1 key *or* Ctrl+Shift+P *or* View -> Command Palette)
+    - Run `Remote-Containers: Clone Repository in Container Volume ...`
+    - Select GitHub
+    - Type `python-automated-scraper` and select **your-account/python-automated-scraper** repository
+    - Select **main** branch
+    - Your repo will be ready when the container is built
+1. Create [Google spreadsheet](https://docs.google.com/spreadsheets/u/0/?tgif=d) in your Drive folder as a destination for scraped data
+    - Name it `Python to Sheets`
+1. Authorize your scraper application to edit destination spreadsheet. Follow the instructions [here](https://pygsheets.readthedocs.io/en/latest/authorization.html).
+    - Use second option - Service Account
+    - Download your `.json` file - the content will be passed to the development container using an Environment variable
+    - Note the generated email associated with created Service Account. It is also available in downloaded config.json file under `client_email` key.
+1. In your `Python to Sheets` share the spreadsheet with Service Account by adding the generated email as Editor
+![share screenshot](img/share-spreadsheet-with-service-acc.png?raw=true)
+1. In your forked Github repository create new Action secret named `GDRIVE_API_CREDENTIALS` and copy the content of downloaded `.json` file from step 5 as **Value** for this secret.
--- python/scraper.py
+++ python/scraper.py
@@ -7,10 +7,18 @@ from bs4 import BeautifulSoup
 import requests as r
 import pandas as pd
 import re
-# import json
 import jsonpickle
+import pygsheets
 
 jsonpickle.set_encoder_options('json', indent=4, ensure_ascii=False)
+initial_load = False
+
+# Google sheet (database)
+gc = pygsheets.authorize(service_account_env_var = 'GDRIVE_API_CREDENTIALS')
+gsheet = gc.open('Python to Sheets') # get using name
+# gsheet = gc.open_by_key('14-b6PRiyY4lb72n3_vqZAvFsm8ybwi4Bgnc89AX5Zz8') # get using google sheet key (preferred)
+gworksheet = gsheet.worksheet_by_title('Najave')
+# gworksheet = gsheet.worksheet_by_title('test')
 
 class NewsItem:
     def __init__(self, title, url, date):
@@ -20,15 +28,15 @@ class NewsItem:
         self.full_text = ""
 
 ## Najave
-def get_news_item_headers(base_url, pojam, stop_url):
+def get_news_item_headers(url_template, pojam, stop_url):
     
     parsed_najave = list()
-    print("Reading from page ...")
+    print("Reading headers from page ...")
 
     for page_num in range(1, 3):
 
         # build url
-        najave_url = base_url.replace("#pojam", pojam).replace("#pgNum", str(page_num))
+        najave_url = url_template.replace("#pojam", pojam).replace("#pgNum", str(page_num))
         print(najave_url)
 
         # get html from url
@@ -48,7 +56,7 @@ def get_news_item_headers(base_url, pojam, stop_url):
 
             item = NewsItem(
                 title = title_element.text,
-                url = url_element["href"],
+                url = base_url + url_element["href"],
                 date = date_element.text.split(' | ')[0]
             )
 
@@ -66,27 +74,74 @@ def get_news_item_headers(base_url, pojam, stop_url):
 
     return parsed_najave
 
-def get_najava_body(url):
+def get_news_item_body(url):
+    print("Reading body from: " + url)
     # get html from url
-    najava_url = base_url + url
-    najava = r.get(najava_url)
+    najava = r.get(url)
     soup = BeautifulSoup(najava.content, "html.parser")
     najava_text = soup.find("div", class_="page_content").text
     return najava_text
 
+def gsheet_get_table(g_worksheet) -> pd.DataFrame:
+    print("Reading worksheet: " + g_worksheet.title)
+    # get the values of sheet as dataframe
+    df = g_worksheet.get_as_df()
+    # print(df)
+    return df
+
 # START
+last_existing_news_item_id = ''
 base_url = 'https://vlada.gov.hr'
 najave_base_url = base_url + '/vijesti/8?trazi=1&tip=3&tip2=&tema=&profil=&datumod=&datumdo=&pojam=#pojam&page=#pgNum'
+
+
+existing_spreadsheet_data = gsheet_get_table(gworksheet)
+
+existing_row_count = existing_spreadsheet_data.shape[0]
+
+if existing_row_count == 0:
+    print("Initial run!")
+    initial_load = True
+    # insert header row
+    header_row = ["Naslov", "Datum", "URL", "Sadrzaj"]
+    gworksheet.update_row(1, header_row)
+    # make it bold
+    a1_cell = gworksheet.cell('A1')
+    a1_cell.set_text_format('bold', True)
+    a1_cell.color = (0.8,0.8,0.8,0.1)
+    pygsheets.DataRange('B1','D1', worksheet=gworksheet).apply_format(a1_cell)
+
+else:
+    print("Found " + str(existing_row_count) + " rows")
+    # convert the 'Datum' column to datetime format
+    existing_spreadsheet_data['Datum']= pd.to_datetime(existing_spreadsheet_data['Datum'])
+    existing_spreadsheet_data = existing_spreadsheet_data.sort_values(by=['Datum'])
+    # get url from last saved record
+    last_existing_news_item_id = existing_spreadsheet_data['URL'].iloc[-1]
+    print("Latest row is:")
+    print(existing_spreadsheet_data.iloc[-1:])
+
 # get headers as list
-najave_list = get_news_item_headers(najave_base_url, "sjednica", "/vijesti/82-sjednica-vlade/33251")
+najave_list = get_news_item_headers(najave_base_url, "sjednica", last_existing_news_item_id)
+
+# print(jsonpickle.encode(najave_list, unpicklable=False))
+
 
 for n in najave_list:
-    n.full_text = get_najava_body(n.url)
-
-print(jsonpickle.encode(najave_list, unpicklable=False))
+    n.full_text = get_news_item_body(n.url) # scrape content
 
 # convert to DataFrame
-# najave_df = pd.read_json(jsonpickle.encode(najave_list, unpicklable=False))
+new_records_count = len(najave_list)
+
+if new_records_count > 0:
+    najave_df = pd.read_json(jsonpickle.encode(najave_list, unpicklable=False))
+    najave_df['date']= pd.to_datetime(najave_df['date'])
+    najave_df = najave_df.sort_values(by=['date'])
+    print("Writing new rows: " + str(new_records_count))
+    new_data_range_start = "A" + str(existing_row_count + 2)
+    gworksheet.set_dataframe(najave_df, new_data_range_start, copy_head=False)
+else:
+    print("No new rows found!")
 
 # print(najave_df)
 
